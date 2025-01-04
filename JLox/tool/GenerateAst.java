6057fc9e5f1acb0abf49fd85e07d6a26d6f4bb7c
VladanT3 <vladt.t33@gmail.com>
2025-01-04 20:27:10 +0100

    5.3; working with trees

--- JLox/tool/GenerateAst.java
+++ JLox/tool/GenerateAst.java
@@ -25,14 +25,26 @@ public class GenerateAst {
 
         writer.println("package lox;\n");
         writer.println("import java.util.List;\n");
-        writer.print("abstract class " + base_name + " {");
+        writer.println("abstract class " + base_name + " {");
+        defineVisitor(writer, base_name, types);
         for (String type : types) {
             String class_name = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, base_name, class_name, fields);
         }
+        writer.println("\n\tabstract <R> R accept(Visitor<R> visitor);");
         writer.print("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String base_name, List<String> types) {
+        writer.println("\tinterface Visitor<R> {");
+        for (String type : types) {
+            String type_name = type.split(":")[0].trim();
+            writer.println(
+                    "\t\tR visit" + type_name + base_name + "(" + type_name + " " + base_name.toLowerCase() + ");\n");
+        }
+        writer.println("\t}");
     }
 
     private static void defineType(PrintWriter writer, String base_name, String class_name, String field_list) {
@@ -45,6 +57,11 @@ public class GenerateAst {
             writer.println("\t\t\tthis." + name + " = " + name + ";");
         }
 
+        writer.println("\t\t}\n");
+
+        writer.println("\t\t@Override");
+        writer.println("\t\t<R> R accept(Visitor<R> visitor) {");
+        writer.println("\t\t\treturn visitor.visit" + class_name + base_name + "(this);");
         writer.println("\t\t}\n");
 
         for (String field : fields) {
