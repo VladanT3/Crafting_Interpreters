0a6f68a9f9a96affb2fe8f545cab82aca435431b
VladanT3 <vladt.t33@gmail.com>
2025-01-06 19:07:58 +0100

    7.3; runtime errors

--- JLox/lox/Interpreter.java
+++ JLox/lox/Interpreter.java
@@ -19,6 +19,7 @@ class Interpreter implements Expr.Visitor<Object> {
 			case BANG:
 				return !isTruthy(right);
 			case MINUS:
+				checkNumberOperand(expr.operator, right);
 				return -(double) right;
 		}
 
@@ -32,23 +33,31 @@ class Interpreter implements Expr.Visitor<Object> {
 
 		switch (expr.operator.type) {
 			case MINUS:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left - (double) right;
 			case SLASH:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left / (double) right;
 			case STAR:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left * (double) right;
 			case PLUS:
 				if (left instanceof Double && right instanceof Double)
 					return (double) left + (double) right;
 				if (left instanceof String && right instanceof String)
 					return (String) left + (String) right;
+				throw new RuntimeError(expr.operator, "Operands must be two strings or two numbers.");
 			case GREATER:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left > (double) right;
 			case GREATER_EQUAL:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left >= (double) right;
 			case LESS:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left < (double) right;
 			case LESS_EQUAL:
+				checkNumberOperands(expr.operator, left, right);
 				return (double) left <= (double) right;
 			case BANG_EQUAL:
 				return !isEqual(left, right);
@@ -57,6 +66,18 @@ class Interpreter implements Expr.Visitor<Object> {
 		}
 
 		return null;
+	}
+
+	private void checkNumberOperand(Token operator, Object operand) {
+		if (operand instanceof Double)
+			return;
+		throw new RuntimeError(operator, "Operand must be a number.");
+	}
+
+	private void checkNumberOperands(Token operator, Object left, Object right) {
+		if (left instanceof Double && right instanceof Double)
+			return;
+		throw new RuntimeError(operator, "Operands must be numbers.");
 	}
 
 	private boolean isEqual(Object a, Object b) {
