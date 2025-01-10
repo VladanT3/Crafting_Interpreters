af0859e2a304340e02d1054ac8b43c691c77f436
VladanT3 <vladt.t33@gmail.com>
2025-01-10 16:57:26 +0100

    11.4; interpreting resolved variables

--- JLox/lox/Interpreter.java
+++ JLox/lox/Interpreter.java
@@ -2,10 +2,13 @@ package lox;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	final Environment globals = new Environment();
 	private Environment environment = globals;
+	private final Map<Expr, Integer> locals = new HashMap<>();
 
 	Interpreter() {
 		globals.define("clock", new LoxCallable() {
@@ -176,13 +179,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Object visitVariableExpr(Expr.Variable expr) {
-		return environment.get(expr.name);
+		return lookupVariable(expr.name, expr);
 	}
 
 	@Override
 	public Object visitAssignExpr(Expr.Assign expr) {
 		Object value = evaluate(expr.value);
-		environment.assign(expr.name, value);
+
+		Integer distance = locals.get(expr);
+		if (distance != null)
+			environment.assignAt(distance, expr.name, value);
+		else
+			globals.assign(expr.name, value);
+
 		return value;
 	}
 
@@ -268,5 +277,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 			value = evaluate(stmt.value);
 
 		throw new Return(value);
+	}
+
+	void resolve(Expr expr, int depth) {
+		locals.put(expr, depth);
+	}
+
+	private Object lookupVariable(Token name, Expr expr) {
+		Integer distance = locals.get(expr);
+		if (distance != null)
+			return environment.getAt(distance, name.lexeme);
+		else
+			return globals.get(name);
 	}
 }
