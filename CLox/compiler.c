8f8f8a5fa3e5927007f71c481714c40598f1f03a
VladanT3 <vladt.t33@gmail.com>
2025-01-17 13:53:05 +0100

    17; compiling expressions

--- CLox/compiler.c
+++ CLox/compiler.c
@@ -3,21 +3,9 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
+bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
-	int line = -1;
-	for (;;) {
-		Token token = scanToken();
-		if (token.line != line) {
-			printf("%4d ", token.line);
-			line = token.line;
-		} else {
-			printf("\t| ");
-		}
-		printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-		if (token.type == TOKEN_EOF) {
-			break;
-		}
-	}
+	advance();
+	expression();
+	consume(TOKEN_EOF, "Expect end of expression.");
 }
