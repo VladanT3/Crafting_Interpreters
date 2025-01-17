6df90391735d9567acc8b1b1b6b9c06f11fd7ee3
VladanT3 <vladt.t33@gmail.com>
2025-01-17 15:02:51 +0100

    17.3; emitting bytecode

--- CLox/compiler.c
+++ CLox/compiler.c
@@ -13,6 +13,12 @@ typedef struct {
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+	return compilingChunk;
+}
+
 static void errorAt(Token* token, const char* message) {
 	if (parser.panic_mode) {
 		return;
@@ -60,8 +66,26 @@ static void consume(TokenType type, const char* message) {
 	errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+	writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitReturn() {
+	emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+	emitReturn();
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+	emitByte(byte1);
+	emitByte(byte2);
+}
+
 bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
+	compilingChunk = chunk;
 
 	parser.had_error = false;
 	parser.panic_mode = false;
@@ -69,5 +93,7 @@ bool compile(const char* source, Chunk* chunk) {
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expect end of expression.");
+
+	endCompiler();
 	return !parser.had_error;
 }
