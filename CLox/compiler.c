06862be9fc6f8dd831b074544f718e8cc9ab40ac
VladanT3 <vladt.t33@gmail.com>
2025-01-19 18:43:23 +0100

    bug fixes

--- CLox/compiler.c
+++ CLox/compiler.c
@@ -60,7 +60,7 @@ static void errorAt(Token* token, const char* message) {
 		fprintf(stderr, " at end");
 	} else if (token->type == TOKEN_ERROR) {
 	} else {
-		fprintf(stderr, " at '%*.s'", token->length, token->start);
+		fprintf(stderr, " at '%.*s'", token->length, token->start);
 	}
 
 	fprintf(stderr, ": %s\n", message);
@@ -181,7 +181,7 @@ static void unary() {
 static void binary() {
 	TokenType operator_type = parser.previous.type;
 	ParseRule* rule = getRule(operator_type);
-	parsePrecedence((Precedence) rule->precedence + 1);
+	parsePrecedence((Precedence) (rule->precedence + 1));
 
 	switch (operator_type) {
 		case TOKEN_PLUS:
@@ -246,13 +246,13 @@ ParseRule rules[] = {
 	[TOKEN_SLASH] = { NULL, binary, PREC_FACTOR },
 	[TOKEN_STAR] = { NULL, binary, PREC_FACTOR },
 	[TOKEN_BANG] = { unary, NULL, PREC_NONE },
-	[TOKEN_BANG_EQUAL] = { NULL, binary, PREC_NONE },
+	[TOKEN_BANG_EQUAL] = { NULL, binary, PREC_EQUALITY },
 	[TOKEN_EQUAL] = { NULL, NULL, PREC_NONE },
-	[TOKEN_EQUAL_EQUAL] = { NULL, binary, PREC_NONE },
-	[TOKEN_GREATER] = { NULL, binary, PREC_NONE },
-	[TOKEN_GREATER_EQUAL] = { NULL, binary, PREC_NONE },
-	[TOKEN_LESS] = { NULL, binary, PREC_NONE },
-	[TOKEN_LESS_EQUAL] = { NULL, binary, PREC_NONE },
+	[TOKEN_EQUAL_EQUAL] = { NULL, binary, PREC_EQUALITY },
+	[TOKEN_GREATER] = { NULL, binary, PREC_COMPARISON },
+	[TOKEN_GREATER_EQUAL] = { NULL, binary, PREC_COMPARISON },
+	[TOKEN_LESS] = { NULL, binary, PREC_COMPARISON },
+	[TOKEN_LESS_EQUAL] = { NULL, binary, PREC_COMPARISON },
 	[TOKEN_IDENTIFIER] = { NULL, NULL, PREC_NONE },
 	[TOKEN_STRING] = { NULL, NULL, PREC_NONE },
 	[TOKEN_NUMBER] = { number, NULL, PREC_NONE },
